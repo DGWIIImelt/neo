922e5b07567293e2269f91b25623361125881845
DGWIIImelt <douglas.wright@meltwater.com>
2022-04-20 11:38:42 -0400

    break test data prep and euclidian triangle code into separate blocks, move calc logic back into OH class

--- src/handlers/OverHead.ts
+++ src/handlers/OverHead.ts
@@ -1,15 +1,17 @@
 import { TLEapi } from '../api/tle';
 import { UserAddressApi } from '../api/userAdress';
+// import LatLon, { Nvector, Dms } from 'geodesy/latlon-nvector-spherical.js';
 const satellite = require('satellite.js');
-export class OverHead{
+// import { triangle } from '../index';
+export class OverHead {
   action: string = '';
-  distanceAtoB: number = undefined;
+  distanceAtoB: number = undefined; //used both for distance between user and sat as well as distnace between the same sat over a period of time
   query: string = '';
   userCoord: number[] = [null, null];
   satCoord: number[] = [null, null];
   satData;
 
-  constructor(action: string, query: string){
+  constructor (action: string, query: string) {
     this.action = action;
     if(query){
       this.query = query;
@@ -54,9 +56,15 @@ export class OverHead{
     this.satData = await TLEapi.search(this.query);
   }
 
-  async setSatCoord () : Promise<void> {
+  async setSatCoord (coords?: number[]) : Promise<void> {
+    if(coords != undefined){
+      this.satCoord = coords;
+      return;
+    }
+
     if(this.query){
       this.satCoord = await TLEapi.getSatLatLon(this.query);
+      return;
     }
   }
 
@@ -66,11 +74,11 @@ export class OverHead{
     }
   }
 
-  async setUserCoord () : Promise<void> {
-    this.userCoord = await UserAddressApi.getUserAddressViaIP();
+  async setUserCoord (coords?: number[]) : Promise<void> {
+    this.userCoord = coords != undefined ? coords : await UserAddressApi.getUserAddressViaIP();
   }
 
-  setSatCoordLocal(hourOffset: number = 0){
+  setSatCoordLocal (hourOffset: number = 0) {
     const { line1, line2 } = this.satData;
     const satrec = satellite.twoline2satrec(line1, line2);
     const now : Date = new Date();
@@ -97,5 +105,56 @@ export class OverHead{
       satellite.degreesLong(satelliteGround.longitude)
     ];
 
+  }
+
+  setSatOrbitCoords (SCs: object[]) {
+    // presumes an array of coords sorted by thier proximity to the user coord returns the closest SC1 and a coord next to the closest
+    const SC1 : object = SCs[0];
+    let SC2 : object;
+
+    // trying to avoid overflowing the array
+    // order = the index of when the coords where taken in the orbital path, 0 being the first
+    if(SC1['order'] === 0){
+      SC2 = SCs.find((el) => el['order'] == 1);
+    }else if(SC1['order'] === SCs.length - 1){
+      SC2 = SCs.find((el) => el['order'] == SC1['order'] - 1);
+    }else{
+      SC2 = SCs.find((el) => el['order'] == SC1['order'] + 1);
+    }
+
+    return {SC1, SC2};
+  }
+
+  setAngleEuclidean (A: number, B: number, C: number): number {
+    // sets angles of a triangle based on side lengths, presumes a flat surface so might not be accurate enough for this task
+    // let thingy = setAngleEuclidean(8, 5, 10); // TEST equals 52.41 https://www.youtube.com/watch?v=COMiK1L0Oj8
+      // console.log('Thingy: ', thingy)
+    const sides : number = Math.pow(A, 2) - Math.pow(B, 2) - Math.pow(C, 2);
+    const angRad : number = Math.acos(sides / (-2 * B * C));
+    const angDeg : number = angRad * 180 / Math.PI; // rad > deg
+
+    return angDeg;
+  }
+
+  //TODO
+  // setClosestCoordInOrbit (triangle: triangle): {lat: number, long: number} {
+    // console.log(triangle)
+    // cicumference of Earth 40075km
+    // take first sat coord, user coord, 
+    // calculate second with distance from user to first coord, distance from first coord to second
+    // make triangle with first to la
+
+    // OR
+    // calculate all points in a circle around the earth, following the path of the orbit
+    // binary search it, checking if distance from a point on the circle is greater/lesser than a previous one to the user coords, using haversine each time
+
+    // OR
+    // http://www.movable-type.co.uk/scripts/latlong-vectors.html#:~:text=source%20code%20below.-,Cross%2Dtrack%20distance,-The%20cross%2Dtrack
+  //   return {lat: 0, long: 0};
+  // }
+
+  //TODO
+  setAngleElliptical (): number {
+    return 0;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
 import { OverHead } from './handlers/OverHead';
 const testData = require('../data/test.json');
 
+export interface triangle {
+  UserCoord:{
+    coords: number[],
+    angle: number,
+    distance: {
+      SatCoord1: number,
+      SatCoord2: number
+    }
+  },
+  SatCoord1:{
+    coords: number[],
+    angle: number,
+    distance: {
+      UserCoord: number,
+      SatCoord2: number
+    }
+  },
+  SatCoord2:{
+    coords: number[],
+    angle: number,
+    distance: {
+      SatCoord1: number,
+      UserCoord: number
+    }
+  }
+}
+
 (async() => {
   const OH = new OverHead(process.argv[2], process.argv[3]);
 
@@ -82,78 +109,17 @@ const testData = require('../data/test.json');
       // how to choose the two points to create the line? grab the two points with an x or y value that is on both sides of the user's x or y
       // todo rewrite setdistance to take parameters and to loop through the coords array OR from any point making a line of coords
       // todo if satellite is 100 miles from viewer OR if satellite is viewable, taking into account horizon https://en.wikipedia.org/wiki/Horizon
+      // moving logic to "test" case to save the API hits
       break;
 
-    // trying to find the closest point on an orbital path to the user
     case "test":
-      const { features } = testData;
-      const testUserCoord = features.find((el: object) => el['label'] == 'user').geometry.coordinates;
-      const testSatCoords = features
-        .filter((el: object) => el['label'] == 'satellite')
-        .map((el: object) => [el['geometry'].coordinates[1], el['geometry'].coordinates[0]]); // values were swapped for use with geojson.io
-      OH.userCoord = [testUserCoord[1], testUserCoord[0]]; // values were swapped for use with geojson.io
-      const distances : object[] = [];
-      let triangle : object = {
-        U:{
-          coords: [],
-          angle: undefined,
-          distance: {
-            SC1: undefined,
-            SC2: undefined
-          }
-        },
-        SC1:{
-          coords: [],
-          angle: undefined,
-          distance: {
-            U: undefined,
-            SC2: undefined
-          }
-        },
-        SC2:{
-          coords: [],
-          angle: undefined,
-          distance: {
-            SC1: undefined,
-            U: undefined
-          }
-        }
-      };
-
-      testSatCoords.forEach((el: [], index: number) => {
-        OH.satCoord = el;
-        OH.setDistance();
-        distances.push({distance: OH.distanceAtoB, order: index, coords: OH.satCoord});
-      });
-
-      distances.sort((a: object, b:object) => a['distance'] - b['distance']);
-
-      const { SC1, SC2 } = setSatOrbitCoords(distances);
-      OH.setDistance(SC1['coords'], SC2['coords']);
-      const SC1toSC2dist = OH.distanceAtoB;
-
-      triangle['U']['coords'] = OH.userCoord;
-      triangle['U']['distance'].SC1 = SC1['distance'];
-      triangle['U']['distance'].SC2 = SC2['distance'];
-
-      triangle['SC1']['coords'] = SC1['coords'];
-      triangle['SC1']['distance'].U = SC1['distance'];
-      triangle['SC1']['distance'].SC2 = SC1toSC2dist;
-
-      triangle['SC2']['coords'] = SC2['coords'];
-      triangle['SC2']['distance'].U = SC2['distance'];
-      triangle['SC2']['distance'].SC1 = SC1toSC2dist;
-
-      triangle['U']['angle'] = setAngleEuclidean(triangle['SC1']['distance'].SC2, triangle['SC1']['distance'].U, triangle['SC2']['distance'].U);
-      triangle['SC1']['angle'] = setAngleEuclidean(triangle['SC2']['distance'].U, triangle['SC1']['distance'].U, triangle['SC2']['distance'].SC1);
-      triangle['SC2']['angle'] = setAngleEuclidean(triangle['SC1']['distance'].U, triangle['SC2']['distance'].U, triangle['SC2']['distance'].SC1);
-
-      // ---------------------------- MATH ATTEMPT BELOW, PRESUMING FLAT SURFACE ----------------------------
-      // Calculate angles
-
-      console.log(triangle)
-      // let thingy = setAngleEuclidean(8, 5, 10); // TEST equals 52.41 https://www.youtube.com/watch?v=COMiK1L0Oj8
-      // console.log('Thingy: ', thingy)
+      // trying to find the closest point on an orbital path to the user
+      const data = setTestData(testData.features);
+      console.log(data)
+      // const euclideanTriangle = setEuclideanTriangle(data); // works for flat surfaces or short distances on a curved surface
+      // const ellipticalTriangle = setElliptialTriangle(data);
+      // todo
+      // using data const above find the point on the orbit that is closest to the user, not the closest set of satCoords but a new coord that falls on the orbital path
       break;
 
     case "getSatPropagate":
@@ -168,51 +134,80 @@ const testData = require('../data/test.json');
       console.log(OH.satData);
       break;
   }
-})()
 
-function setSatOrbitCoords(SCs: object[]){
-  // presumes an array of coords sorted by thier proximity to the user coord
-  const SC1 : object = SCs[0];
-  let SC2 : object;
 
-  // trying to avoid overflowing the array
-  if(SC1['order'] === 0){
-    SC2 = SCs.find((el) => el['order'] == 1);
-  }else if(SC1['order'] === SCs.length - 1){
-    SC2 = SCs.find((el) => el['order'] == SC1['order'] - 1);
-  }else{
-    SC2 = SCs.find((el) => el['order'] == SC1['order'] + 1);
+  function setTestData (data: any[]) : object[] {
+    // pulls in test data, reorgs it a bit and runs some calcs and returns an array of satCoords with distances to the user calculated
+    const testUserCoord = data.find((el: object) => el['label'] == 'user').geometry.coordinates;
+    const testSatCoords = data
+      .filter((el: object) => el['label'] == 'satellite')
+      .map((el: object) => [el['geometry'].coordinates[1], el['geometry'].coordinates[0]]); // values were swapped for use with geojson.io
+    OH.setUserCoord([testUserCoord[1], testUserCoord[0]]); // values were swapped for use with geojson.io
+    const distances : object[] = [];
+
+    testSatCoords.forEach((el: [], index: number) => {
+      OH.setSatCoord(el);
+      OH.setDistance(); // distance from this point in the sat orbit to the user
+      distances.push({distance: OH.distanceAtoB, order: index, coords: OH.satCoord});
+    });
+
+    return distances;
   }
 
-  return {SC1, SC2};
-}
+  // function setElliptialTriangle (data: any[]) : triangle {
 
-function setAngleEuclidean(A: number, B: number, C: number): number {
-  // sets angles of a triangle based on side lengths, presumes a flat surface so might not be accurate enough for this task
-  const sides : number = Math.pow(A, 2) - Math.pow(B, 2) - Math.pow(C, 2);
-  const angRad : number = Math.acos(sides / (-2 * B * C));
-  const angDeg : number = angRad * 180 / Math.PI; // rad > deg
+  // }
 
-  return angDeg;
-}
+  // function setEuclideanTriangle (distances: object[]) : triangle {
+  //   distances.sort((a: object, b:object) => a['distance'] - b['distance']);
 
-//TODO
-function setClosestCoordInOrbit(triangle): {lat: number, long: number} {
-  // cicumference of Earth 40075km
-  // take first sat coord, user coord, 
-  // calculate second with distance from user to first coord, distance from first coord to second
-  // make triangle with first to la
+  //   const { SC1, SC2 } = OH.setSatOrbitCoords(distances);
+  //   OH.setDistance(SC1['coords'], SC2['coords']);
+  //   const SC1toSC2dist = OH.distanceAtoB;
 
-  // OR
-  // calculate all points in a circle around the earth, following the path of the orbit
-  // binary search it, checking if distance from a point on the circle is greater/lesser than a previous one to the user coords, using haversine each time
+  //   let triangle : triangle = {
+  //     UserCoord:{
+  //       coords: OH.userCoord,
+  //       angle: undefined,
+  //       distance: {
+  //         SatCoord1: SC1['distance'],
+  //         SatCoord2: SC2['distance']
+  //       }
+  //     },
+  //     SatCoord1:{
+  //       coords: SC1['coords'],
+  //       angle: undefined,
+  //       distance: {
+  //         UserCoord: SC1['distance'],
+  //         SatCoord2: SC1toSC2dist
+  //       }
+  //     },
+  //     SatCoord2:{
+  //       coords: SC2['coords'],
+  //       angle: undefined,
+  //       distance: {
+  //         SatCoord1: SC1toSC2dist,
+  //         UserCoord: SC2['distance']
+  //       }
+  //     }
+  //   };
 
-  // OR
-  // http://www.movable-type.co.uk/scripts/latlong-vectors.html#:~:text=source%20code%20below.-,Cross%2Dtrack%20distance,-The%20cross%2Dtrack
-  return {lat: 0, long: 0};
-}
+  //   triangle.UserCoord.angle = OH.setAngleEuclidean(
+  //     triangle.SatCoord1.distance.SatCoord2,
+  //     triangle.SatCoord1.distance.UserCoord,
+  //     triangle.SatCoord2.distance.UserCoord
+  //   );
+  //   triangle.SatCoord1.angle = OH.setAngleEuclidean(
+  //     triangle.SatCoord2.distance.UserCoord,
+  //     triangle.SatCoord1.distance.UserCoord,
+  //     triangle.SatCoord2.distance.SatCoord1
+  //   );
+  //   triangle.SatCoord2.angle = OH.setAngleEuclidean(
+  //     triangle.SatCoord1.distance.UserCoord,
+  //     triangle.SatCoord2.distance.UserCoord,
+  //     triangle.SatCoord2.distance.SatCoord1
+  //   );
 
-//TODO
-function setAngleElliptical(): number {
-  return 0;
-}
+  //   return triangle;
+  // }
+})()
